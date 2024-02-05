58faabf2ef89d2491b160636d9c89e3597b805fd
nicopressez <154272808+nicopressez@users.noreply.github.com>
2024-02-05 16:08:13 +0100

    Added form responses based on JSON status

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,13 @@
 import { useState, useContext } from "react";
 import { AuthContext } from "../App";
+import { Link } from "react-router-dom";
 
 const Form = () => {
    const token =localStorage.getItem("token")
 
    const [posted, setPosted] = useState(false);
    const [admin, setAdmin] = useState(true);
-   const [errors, setErrors] = useState([]);
+   const [errors, setErrors] = useState();
 
    const { auth, setAuth } = useContext(AuthContext);
 
@@ -32,19 +33,24 @@ const Form = () => {
          const result = await response.json()
  // Handle response based on JSON status
          switch (status) {
-            case 200: setPosted(true);
+            case 200: {
+               setPosted(true);
+               setErrors()}
                break;
 // If form not filled out correctly        
             case 400: setErrors(await result.errors)
                break;
 // If no token or it expired
             case 404: {
+               setErrors();
                localStorage.clear();
                setAuth(false);
             }
                break;
 // If not admin
-            case 403: setAdmin(false);
+            case 403: {
+               setAdmin(false);
+               setErrors();}
                break;
          }
       } catch(err) {
@@ -66,7 +72,22 @@ const Form = () => {
       <input type="checkbox" name="visible"></input>
       <input type="submit" value="Submit"></input>
    </form> 
-    : ""}
+    : posted ?
+    <div>
+   <h1>Blog post added!</h1> 
+   <Link to={"/"}>Back to homepage</Link>
+    </div>
+    : !admin ?
+   <h1>You don't have the required permissions for this</h1>
+    : 
+   <h1>Your session has expired, please log back in</h1>}
+
+   {errors ? 
+   <ul> Fix the following errors before submitting:
+      {errors.map((error, index) => (
+      <li key={index}>{error.msg}</li>) )}
+   </ul> :
+   ""}
       
     </div>
  )
